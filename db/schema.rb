16e63bee6788853675c8c146a27e6dff1d29e0a0
LouisTheLeaf <lherna1682@gmail.com>
2021-09-30 18:25:42 -0500

    9/30/2021 - Setting price precision

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_09_03_162848) do
     t.string "title"
     t.string "author"
     t.float "price"
-    t.date "published_date"
+    t.date "published_date", precision: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
