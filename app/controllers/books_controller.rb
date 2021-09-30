7a36dd75b43dd2d0434067dabc0f5f09b52e06dd
LouisTheLeaf <lherna1682@gmail.com>
2021-09-30 10:31:11 -0500

    9/30/2021 - Heroku pipeline, new book failure V2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,6 @@ class BooksController < ApplicationController
 
   # GET /books/1 or /books/1.json
   def show
-    @book=Book.find(params[:id]) #after Heroku didn't work
   end
 
   # GET /books/new
@@ -18,7 +17,6 @@ class BooksController < ApplicationController
 
   # GET /books/1/edit
   def edit
-    @book=Book.find(params[:id]) #after Heroku didn't work
   end
 
   # POST /books or /books.json
@@ -27,7 +25,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to books_path, notice: "Book was successfully created." }
+        format.html { redirect_to @book, notice: "Book was successfully created." }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +38,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to books_path, notice: "Book was successfully updated." }
+        format.html { redirect_to @book, notice: "Book was successfully updated." }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -49,19 +47,13 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
-    @book=Book.find(params[:id])
-  end
   # DELETE /books/1 or /books/1.json
   def destroy
-    @book=Book.find(params[:id])
     @book.destroy
-    flash["notice"]="Book '#{@book.title}' deleted successfully."
-    redirect_to(books_path)
-#    respond_to do |format|
-#      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
-#      format.json { head :no_content }
-#    end
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
