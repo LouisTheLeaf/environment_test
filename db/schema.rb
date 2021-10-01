85deead2e8a256e932dd6655a0ec74aeb2c33b04
LouisTheLeaf <lherna1682@gmail.com>
2021-09-30 19:10:16 -0500

    9/30/2021 - Fixing some attributes and tags

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 2021_09_03_162848) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
-    t.float "price"
-    t.date "published_date", precision: 2
+    t.numerical "price", precision: 2
+    t.date "published_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
